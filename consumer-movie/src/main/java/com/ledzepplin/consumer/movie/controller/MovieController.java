b39d99f50986212a5ce8157e2d9859cfc32c806e
jingjiadong <jingjiadong@baiwang.com>
2018-01-09 00:34:25 +0800

    hystrix first

--- consumer-movie/src/main/java/com/ledzepplin/consumer/movie/controller/MovieController.java
+++ consumer-movie/src/main/java/com/ledzepplin/consumer/movie/controller/MovieController.java
@@ -2,6 +2,8 @@ package com.ledzepplin.consumer.movie.controller;
 
 import com.ledzepplin.consumer.movie.entity.User;
 import com.ledzepplin.consumer.movie.feign.UserFeignClient;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,18 +37,32 @@ public class MovieController {
     private UserFeignClient userFeignClient;
 
 
-    @GetMapping("/user/{id}")
-    public User findByIdAdmin(@PathVariable Long id){
-        return userFeignClient.findById(id);
-    }
 //    @GetMapping("/user/{id}")
-//    public User findById(@PathVariable Long id){
-//        return restTemplate.getForObject("http://microservice-provider-user/"+id,User.class);
+//    public User findByIdAdmin(@PathVariable Long id){
+//        return userFeignClient.findById(id);
 //    }
+    @HystrixCommand(fallbackMethod = "findByIdFallBack",commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "5000"),
+            @HystrixProperty(name = "metrics.rollingStats.timeInMilliseconds",value = "10000")
+    },threadPoolProperties = {
+            @HystrixProperty(name = "coreSize",value = "1"),
+            @HystrixProperty(name = "maxQueueSize",value = "10")
+    })
+    @GetMapping("/user/{id}")
+    public User findById(@PathVariable Long id){
+        return restTemplate.getForObject("http://microservice-provider-user/"+id,User.class);
+    }
 
+    public User findByIdFallBack(Long Id){
+        User user = new User();
+        user.setId(-1L);
+        user.setName("default user");
+        return user;
+    }
     @GetMapping("/log-instance")
     public void logUserInstance(){
         ServiceInstance serviceInstance = loadBalancerClient.choose("microservice-provider-user");
         logger.info("{}:{}:{}",serviceInstance.getServiceId(),serviceInstance.getHost(),serviceInstance.getPort());
     }
+
 }
--- provider-user/src/main/java/com/ledzepplin/provider/user/controller/UserController.java
+++ provider-user/src/main/java/com/ledzepplin/provider/user/controller/UserController.java
@@ -2,15 +2,8 @@ package com.ledzepplin.provider.user.controller;
 
 import com.ledzepplin.provider.user.entity.User;
 import com.ledzepplin.provider.user.mapper.UserRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Collection;
 
 /**
  * @Author: Jingjiadong
@@ -19,23 +12,12 @@ import java.util.Collection;
  */
 @RestController
 public class UserController {
-    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
-
     @Autowired
     private UserRepository userRepository;
 
     @RequestMapping(value = "/{id}",method = RequestMethod.GET)
     public User findById(@PathVariable Long id){
-        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        if(principal instanceof UserDetails){
-            UserDetails user = (UserDetails) principal;
-            Collection<? extends GrantedAuthority> collection = user.getAuthorities();
-            collection.stream().forEach((x)->{
-                logger.info("当前用户是{},角色是{}",user.getUsername(),x.getAuthority());
-            });
-        }else {
-            //do other things
-        }
+
         return userRepository.findOne(id);
     }
 }
